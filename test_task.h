6fcc19b831aca2cec66783f3f85880ebe3067aa5
Ollsa <72123305+Ollsa@users.noreply.github.com>
2022-04-13 20:26:17 +0300

    delete unnecessary defines

--- test_task.h
+++ test_task.h
@@ -3,12 +3,10 @@
 
 #define DEBUG
 
-#define XML_FILENAME        "test.xml"
 #define XML_START_NODE_NAME "start"
 #define XML_KEY_TEXT        "text"
 #define XML_KEY_NUMERIC     "numeric"
 
-#define TLV_FILENAME        "s1.txt"
 #define TLV_BUF_MAX_SIZE    255
 
 typedef enum xml_keyword
